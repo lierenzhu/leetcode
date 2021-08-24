6e280ffcabdd3febbcbf462b2f1651d5b8fd6bb4
zhuyucheng <@zhuyucheng>
2021-08-24 10:50:01 +0800

    leetcode

---
+++ test.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Description: 
+ * @Author: lierenzhu
+ * @Date: 2021-08-24 10:47:08
+ * @LastEditors: lierenzhu
+ * @LastEditTime: 2021-08-24 10:47:09
+ * @FilePath: /leetcode/test.cpp
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "hello git" << endl;
+}
